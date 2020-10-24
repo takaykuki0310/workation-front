de7754ea7cf9fb25578730bd21afc07c3e806ede
Taku Kobayashi <keep_slimbody@yahoo.co.jp>
2020-10-24 17:24:54 +0900

    answerをclassに改修

--- src/components/answer.js
+++ src/components/answer.js
@@ -2,18 +2,25 @@ import React from 'react';
 
 import './style.scss';
 
-const answer = () => (
-	<div>
-      <div className="hero is-primary">
+class answer extends React.Component {
+	constructor(props) {
+	  super(props);
+	}
+
+	render() {	
+		return (
+			<div>
+				<div className="hero is-primary">
 					<div className="hero-body">
 						<div className="container">
-								<p className="title main-question">あなたが行くべきワーケーション先は〇〇です</p>
-								<p><progress className="progress is-info" value="100" max="100">20%</progress></p>
-
+							<p className="title main-question">あなたが行くべきワーケーション先は〇〇です</p>
+							<p><progress className="progress is-info" value="100" max="100">20%</progress></p>
 						</div>
 					</div>
+				</div>
 			</div>
-	</div>
-);
+		);
+	}
+}
 
 export default answer;
